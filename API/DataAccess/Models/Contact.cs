c3b5257955275ed6e7bbf0ee1d134ee4a9ce6435
Philip Smith <trumpet7347@gmail.com>
2020-10-26 18:36:48 -0400

    added required fields

--- API/DataAccess/Models/Contact.cs
+++ API/DataAccess/Models/Contact.cs
@@ -11,24 +11,31 @@ namespace DataAccess.Models
         public int ContactId { get; set; }
 
         [MaxLength(20)]
+        [Required]
         public string FirstName { get; set; }
 
         [MaxLength(20)]
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
         [MaxLength(100)]
+        [Required]
         public string Street { get; set; }
 
         [MaxLength(50)]
+        [Required]
         public string City { get; set; }
 
         public State State { get; set; }
 
         [MaxLength(20)]
+        [Required]
         public string Zip { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
         public ContactFrequency ContactFrequency { get; set; }
