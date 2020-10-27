1b3b025f49f38bf4b487c8dc5a9d4ab21c8d944b
Philip Smith <trumpet7347@gmail.com>
2020-10-27 18:34:33 -0400

    removed uneeded field from contact methods model

--- API/DataAccess/Models/ContactMethod.cs
+++ API/DataAccess/Models/ContactMethod.cs
@@ -3,7 +3,6 @@
     public class ContactMethod
     {
         public int ContactMethodId { get; set; }
-        public string Name { get; set; }
         public string Description { get; set; }
     }
 }
